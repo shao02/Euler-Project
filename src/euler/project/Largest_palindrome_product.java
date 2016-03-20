991824dfb07f3a09ac0c4eaae3eb1e33587963a4
“shao02” <sx238@nyu.edu>
2016-03-20 00:08:12 -0400

    Correct Largest_palindrome_product.class bug

--- src/euler/project/Largest_palindrome_product.java
+++ src/euler/project/Largest_palindrome_product.java
@@ -5,20 +5,21 @@ package euler.project;
 //Find the largest palindrome made from the product of two 3-digit numbers.
 public class Largest_palindrome_product {
 	public static void main(String[] args){
-		
+		System.out.print(sol(3));
 	}
 	
 	static private int sol(int digit){
+		int max = 0;
 		if(digit < 1)
 			return -1;
 		for(int i = (int) (Math.pow(10, digit) -1); i>= Math.pow(10, digit-1); i -- ){
 			for(int j = i; j>= Math.pow(10, digit-1); j -- ){
-				if(isPalin(String.valueOf(i * j))){
-					return i * j;
+				if(isPalin(String.valueOf(i * j)) && max < i * j){
+					max = i * j;
 				}
 			}
 		}
-		return -1;
+		return max;
 	}
 	
 	static private boolean isPalin(String i){
